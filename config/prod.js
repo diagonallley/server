8d8b569d9148b426bb6dcf0c41767c8afc9ee236
diagonallley <007shubham007@gmail.com>
2023-12-01 11:47:29 +0530

    render-env

---
+++ config/prod.js
@@ -0,0 +1,14 @@
+// dev.js 
+module.exports = {
+    "googleClientID": process.env.GOOGLE_CLIENT_ID,
+    "project_id": process.env.PROJECT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_UR,
+    "auth_provider_x509_cert_url": process.env.AUTH,
+    "googleClientSecret": process.env.CLIENT_SECRET,
+    "redirect_uris": process.env.REDIRECT,
+    "javascript_origins": process.env.JS,
+    "mongoURI": process.env.MONGO_URI,
+    "cookieKey": process.env.COOKIE_KEY
+};
+
